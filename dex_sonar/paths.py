f1407fe970fab57f334bbcc4df5a7a5c441deefc
anabolicobsession <pilvorm@gmail.com>
2024-12-30 19:31:40 +0100

    Feat: Add module for creating project files paths

---
+++ dex_sonar/paths.py
@@ -0,0 +1,17 @@
+from os import getcwd
+from os.path import join
+
+from dex_sonar.config import parameters
+
+
+Path = str
+FileName = str
+Component = str
+
+
+def get_path(*components: Component) -> Path:
+    return join(getcwd(), *components)
+
+
+def get_config_path(file_name: FileName) -> Path:
+    return join(getcwd(), parameters.CONFIGS_DIR, file_name)
