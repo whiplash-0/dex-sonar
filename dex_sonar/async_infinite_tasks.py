a1e34374aa52faa30ee99b75afd5d3512a169661
anabolicobsession <pilvorm@gmail.com>
2025-02-25 03:01:17 +0100

    Fix(async-infinite-tasks): Wait for all tasks to complete in case any task was cancelled

--- dex_sonar/async_infinite_tasks.py
+++ dex_sonar/async_infinite_tasks.py
@@ -1,5 +1,9 @@
 import asyncio
+import logging
 from typing import Coroutine, Optional
+
+
+logger = logging.getLogger(__name__)
 
 
 class AsyncInfiniteTasks:
@@ -12,8 +16,11 @@ class AsyncInfiniteTasks:
             self.loop = asyncio.get_event_loop()
             self.tasks = [asyncio.create_task(x) for x in self.tasks]
             await asyncio.gather(*self.tasks)
+
         except asyncio.CancelledError:
-            ...
+            logger.debug('Caught `CancelledError`. Cancelling all tasks and waiting for them to complete')
+            for x in self.tasks: x.cancel()
+            await asyncio.gather(*self.tasks, return_exceptions=True)  # supress exception raising with parameter
 
     def run_coroutine_threadsafe(self, coro: Coroutine):
         asyncio.run_coroutine_threadsafe(coro, self.loop)
