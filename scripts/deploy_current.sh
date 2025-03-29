d15e335c7ceb26d7ba6a0e0eecdfe2107e55519c
anabolicobsession <pilvorm@gmail.com>
2025-03-29 16:17:59 +0100

    Feat(deploy): Make script globally executable

--- scripts/deploy_current.sh
+++ scripts/deploy_current.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-git add *
-git commit -m "Ops: Temporal commit for Heroku deployment"
-./scripts/deploy.sh "$(git branch --show-current)"
-git reset --soft HEAD~1
-git reset
+project_root_path="$(dirname "$(realpath "$0")")/.."
+
+if cd "$project_root_path"; then  # move to project level directory for reproducibility
+  git add *
+  git commit -m "Ops: Temporal commit for Heroku deployment"
+  ./scripts/deploy.sh "$(git branch --show-current)"
+  git reset --soft HEAD~1
+  git reset
+else
+    echo "Failed to change directory to project root"
+    exit 1
+fi
