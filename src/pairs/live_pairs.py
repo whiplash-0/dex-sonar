111dc05a9350b96e1ccf4da46b24e2e961ac73c3
anabolicobsession <pilvorm@gmail.com>
2025-04-09 15:06:10 +0200

    Refactor(live-pairs): Improve code structure

--- src/pairs/live_pairs.py
+++ src/pairs/live_pairs.py
@@ -2,12 +2,15 @@ import logging
 from datetime import datetime, timedelta
 from typing import Callable, Iterable
 
-from pybit.unified_trading import HTTP, WebSocket
+from pybit import unified_trading
 
 from src.pairs.pair import Pair, Symbol, TimeSeries
 from src.pairs.pairs import Pairs
 from src.pairs.pybit_converters import Convert, Response
 from src.utils import time
+
+
+CATEGORY = 'linear'
 
 
 logger = logging.getLogger(__name__)
@@ -26,10 +29,12 @@ class LivePairs(Pairs):
         self.callback_on_update = callback_on_update
         self.include_filter = include_filter
 
-        self.requests = HTTP(testnet=False)
-        self.websocket = WebSocket(
+        self.requests = unified_trading.HTTP(
             testnet=False,
-            channel_type='linear',
+        )
+        self.websocket = unified_trading.WebSocket(
+            testnet=False,
+            channel_type=CATEGORY,
         )
         self.last_update: dict[Symbol, datetime] = {}
 
@@ -39,14 +44,14 @@ class LivePairs(Pairs):
         return self.websocket.is_connected()
 
     def subscribe_to_stream(self):
-        self._update_klines()
-        self.websocket.kline_stream(1, self.get_symbols(), self._handle_kline_update)
+        self._update_candles()
         self.websocket.ticker_stream(self.get_symbols(), self._handle_ticker_update)
+        self.websocket.kline_stream(1, self.get_symbols(), self._handle_kline_update)
 
     def _init(self):
         pairs = Pairs()
 
-        for ticker in Convert.get_tickers(self.requests.get_tickers(category='linear')):
+        for ticker in Convert.get_tickers(self.requests.get_tickers(category=CATEGORY)):
             pairs.update(Pair(
                 symbol=ticker.symbol,
                 prices=TimeSeries(step=timedelta(minutes=1)),
@@ -60,14 +65,58 @@ class LivePairs(Pairs):
         self.update(self.include_filter(pairs))
         self.last_update = {x: time.MIN_TIMESTAMP for x in self.pairs}
 
-    def _update_klines(self):
-        for symbol in self.get_symbols(): self._update_kline(symbol)
+    def _handle_ticker_update(self, response: Response):
+        try:
+            symbol = response['data']['symbol']
 
-    def _update_kline(self, symbol: Symbol):
+            if time.get_timestamp() - self.last_update[symbol] >= self.update_frequency:
+                self.last_update[symbol] = time.get_timestamp()
+                ticker = Convert.stream_ticker(response)
+                pair = self[symbol]
+
+                pair.prices.update(
+                    ticker.price,
+                    time.ceil_timestamp_minute(ticker.timestamp),
+                )
+                pair.update(
+                    turnover=ticker.turnover,
+                    open_interest=ticker.open_interest,
+                    funding_rate=ticker.funding_rate,
+                    next_funding_time=ticker.next_funding_time,
+                )
+
+                self.callback_on_update(pair)
+
+        except Exception:
+            logger.exception('Caught pybit callback exception:'); raise
+
+    def _handle_kline_update(self, response: Response):
+        try:
+            if response['data'][0]['confirm']:
+                kline = Convert.stream_kline(response)
+
+                self[kline.symbol].prices.update(
+                    kline.close,
+                    time.ceil_timestamp_minute(kline.end),
+                    is_final=True,
+                )
+                self[kline.symbol].turnovers.update(
+                    kline.turnover,
+                    time.ceil_timestamp_minute(kline.end),
+                    is_final=True,
+                )
+
+        except Exception:
+            logger.exception('Caught pybit callback exception:'); raise
+
+    def _update_candles(self):
+        for symbol in self.get_symbols(): self._update_candle(symbol)
+
+    def _update_candle(self, symbol: Symbol):
         pair = self[symbol]
         kline = Convert.get_kline(
             self.requests.get_kline(
-                category='linear',
+                category=CATEGORY,
                 symbol=symbol,
                 intervalTime='1',
                 limit=1000,
@@ -96,47 +145,3 @@ class LivePairs(Pairs):
             kline.turnovers[-1],
             kline.starts[-1] + pair.turnovers.get_time_step(),
         )
-
-    def _handle_kline_update(self, response: Response):
-        try:
-            if response['data'][0]['confirm']:
-                kline = Convert.stream_kline(response)
-
-                self[kline.symbol].prices.update(
-                    kline.close,
-                    time.ceil_timestamp_minute(kline.end),
-                    is_final=True,
-                )
-                self[kline.symbol].turnovers.update(
-                    kline.turnover,
-                    time.ceil_timestamp_minute(kline.end),
-                    is_final=True,
-                )
-
-        except Exception:
-            logger.exception('Caught pybit callback exception:'); raise
-
-    def _handle_ticker_update(self, response: Response):
-        try:
-            symbol = response['data']['symbol']
-
-            if time.get_timestamp() - self.last_update[symbol] >= self.update_frequency:
-                self.last_update[symbol] = time.get_timestamp()
-                ticker = Convert.stream_ticker(response)
-                pair = self[symbol]
-
-                pair.prices.update(
-                    ticker.price,
-                    time.ceil_timestamp_minute(ticker.timestamp),
-                )
-                pair.update(
-                    turnover=ticker.turnover,
-                    open_interest=ticker.open_interest,
-                    funding_rate=ticker.funding_rate,
-                    next_funding_time=ticker.next_funding_time,
-                )
-
-                self.callback_on_update(pair)
-
-        except Exception:
-            logger.exception('Caught pybit callback exception:'); raise
