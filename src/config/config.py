2035a3e3757518dc88f784ae165b7482d3b019d5
anabolicobsession <pilvorm@gmail.com>
2025-04-14 20:14:21 +0200

    Chore(config): Add parsing of big numbers to `getint` and `getfloat` methods

--- src/config/config.py
+++ src/config/config.py
@@ -5,10 +5,12 @@ from datetime import timedelta
 from pathlib import Path
 from zoneinfo import ZoneInfo
 
+from src.utils import utils
 from src.utils.paths import Paths
 
 
 DEFAULT_PRESET = 'default'
+
 
 CONFIG = None
 
@@ -28,10 +30,12 @@ class Config(RawConfigParser):
         super().read(self.directory_path / (name + '.ini'), **kwargs)
 
     def getint(self, section, option, default: int = None, **kwargs) -> int | None:
-        return super().getint(section, option, **kwargs) if self.get(section, option, **kwargs) else default
+        if string := self.get(section, option, **kwargs): return utils.parse_large_number(string, as_type=int)
+        else: return default
 
     def getfloat(self, section, option, default: float = None, **kwargs) -> float | None:
-        return super().getfloat(section, option, **kwargs) if self.get(section, option, **kwargs) else default
+        if string := self.get(section, option, **kwargs): return utils.parse_large_number(string, as_type=float)
+        else: return default
 
     def get_timedelta_from_seconds(self, section, option, default: timedelta = None, **kwargs) -> timedelta | None:
         return timedelta(seconds=self.getint(section, option, **kwargs)) if self.get(section, option, **kwargs) else default
